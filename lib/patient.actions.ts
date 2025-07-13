f1c17bb292a9ca0f8eec19b1844d7a9662892be4
dara01x <daramistefa30@gmail.com>
2025-07-13 12:47:41 +0300

    refactor: improve error logging formatting in getUser function

--- lib/patient.actions.ts
+++ lib/patient.actions.ts
@@ -30,8 +30,12 @@ export const createUser = async (user: CreateUserParams) => {
 export const getUser = async (userId: string) => {
   try {
     const user = await users.get(userId);
+
     return parseStringify(user);
   } catch (error) {
-    console.error("An error occurred while retrieving the user details:", error);
+    console.error(
+      "An error occurred while retrieving the user details:",
+      error
+    );
   }
 };
